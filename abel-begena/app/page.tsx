8df8984abe02d69e4307f479aeb6aa394e8ee470
KALU56 <kalkidanbehailu55@gmail.com>
2025-05-31 10:41:34 +0300

    chnage the ui of the contact us

--- abel-begena/app/page.tsx
+++ abel-begena/app/page.tsx
@@ -1,15 +1,17 @@
 import HeroSection from '../components/HeroSection';
 import MissionSection from '../components/MissionSection';
 import AboutSection from '../app/about/page';
-import TrainingSection from '../components/TrainingSection';  // <-- Import here
+import TrainingSection from '../components/TrainingSection';
+import ContactSection from '../components/ContactSection'; // ✅ Import new section
 
 export default function Home() {
   return (
     <div className="min-h-screen">
       <HeroSection />
       <MissionSection />
-      {/* <TrainingSection />  */}
+      {/* <TrainingSection /> */}
       <AboutSection />
+      <ContactSection /> {/* ✅ Add here */}
     </div>
   );
 }
